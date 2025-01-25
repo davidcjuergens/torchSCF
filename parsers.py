23a9e7cba5ac7f2574035dc94e1c67d5f0886a2d
David Juergens <davidcj@stanford.edu>
2025-01-25 14:27:22 -0800

    add parsers.py

---
+++ parsers.py
@@ -0,0 +1,42 @@
+"""File parsing"""
+
+
+def parse_xyz(filename: str):
+    """Parse an xyz file
+
+    XYZ file format:
+
+        -----------------
+        <number of atoms>
+        <comment line>
+        <atom1> <x> <y> <z>
+        <atom2> <x> <y> <z>
+        ...
+        -----------------
+
+    Args:
+        filename (str): The name of the file to parse
+
+    Returns:
+        dict: Dictionary containing
+    """
+    elements, xyz = [], []
+
+    with open(filename, "r") as f:
+
+        for i, line in enumerate(f):
+            if i == 0:
+                natoms = int(line)  # number of atoms
+            elif i == 1:
+                comment = line  # comment line
+            else:
+                element, *coords = line.strip().split()
+                elements.append(element)
+                xyz.append([float(coord) for coord in coords])
+
+    # sanity checks
+    assert (
+        len(elements) == natoms
+    ), f".xyz file says {natoms} atoms, but found {len(elements)} atoms"
+
+    return {"natoms": natoms, "comment": comment, "elements": elements, "xyz": xyz}
